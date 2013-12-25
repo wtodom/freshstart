128734f842e6163273ddd42e22840a8602d7aa3e
Weston Odom <weston.odom@gmail.com>
2013-12-25 13:37:25 -0600

    Fixed typo - python command.

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ cd freshstart
 sudo apt-get update
 sudo apt-get install python3.3
 wget https://bitbucket.org/pypa/setuptools/raw/bootstrap/ez_setup.py
-sudo python 3.3 ez_setup.py
+sudo python3.3 ez_setup.py
 wget https://raw.github.com/pypa/pip/master/contrib/get-pip.py
 sudo python3.3 get-pip.py
 sudo pip install virtualenv virtualenvwrapper
